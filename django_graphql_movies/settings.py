17b4c7c7b344caec69893ca531c64da3b3473fa5
Benedict Kioko <kiokobenedict@gmail.com>
2019-04-23 16:51:42 +0300

    added graphene_django to INSTALLED APPS

--- django_graphql_movies/settings.py
+++ django_graphql_movies/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'graphene_django',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_graphql_movies.movies',
 ]
+
+GRAPHENE = {
+    'SCHEMA': 'django_graphql_movies.schema.schema'
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
