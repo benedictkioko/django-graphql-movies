56750bac9d400c752db4bac41723cb523e7d6c25
Benedict Kioko <kiokobenedict@gmail.com>
2019-04-23 17:02:11 +0300

    registered actor and movie models

--- django_graphql_movies/movies/admin.py
+++ django_graphql_movies/movies/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django_graphql_movies.movies.models import Actor, Movie
 
 # Register your models here.
+admin.site.register(Actor)
+admin.site.register(Movie)
